65890fbd093ba5b41f7e4179193a496da2897977
bluesky <goming49@gmail.com>
2020-05-19 15:08:42 +0800

    create style of components

--- src/components/Tetris.js
+++ src/components/Tetris.js
@@ -1,23 +1,26 @@
 import React from 'react';
 
 import Stage from './Stage'
-import { createStage } from '../gameHelpers'
+import { createStage } from '../gameHelpers';
+import { StyledTetrisWrapper, StyledTetris } from './styles/StyledTetris';
 import Display from './Display'
 import StartButton from './StartButton';
 
 const Tetris = () => {
     return (
-        <div>
-            <Stage stage={createStage()}/>
-            <aside>
-                <div>
-                    <Display text ="Score" />
-                    <Display text ="Rows" />
-                    <Display text ="Level" />
-                </div>
-                <StartButton />
-            </aside>
-        </div>
+        <StyledTetrisWrapper>
+            <StyledTetris>
+                <Stage stage={createStage()}/>
+                <aside>
+                    <div>
+                        <Display text ="Score" />
+                        <Display text ="Rows" />
+                        <Display text ="Level" />
+                    </div>
+                    <StartButton />
+                </aside>
+            </StyledTetris>
+        </StyledTetrisWrapper>
     );
 };
 
